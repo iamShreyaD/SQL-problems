cb9fab6654d1a25f754373321afc918255664c46
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2025-12-11 17:04:38 +0530

    Create 1) Student Advisor.sql

---
+++ 1) Student Advisor.sql
@@ -0,0 +1,7 @@
+A university has started a student-advisor plan which assigns a professor as an advisor to each student for academic guidance. Write a query to find the roll number and names of students who either have a male advisor with a salary of more than 15000 or a female advisor of more than 20000.
+
+SELECT s.roll_number, s.name
+FROM student_information s
+JOIN faculty_information f on s.advisor = f.employee_id
+WHERE (f.gender = 'M' AND f.salary > 15000) 
+    OR (f.gender = 'F' AND f.salary > 20000);
